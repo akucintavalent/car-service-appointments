583630973e4e4ae1c8f3bb317bb65c75ab9c5714
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-22 21:13:52 +0200

    Add validation for startDateTime and endDateTime for appointment model

--- models/appointment.js
+++ models/appointment.js
@@ -12,10 +12,30 @@ const Appointment = sequelize.define('appointment', {
   startDateTime: {
     type: Sequelize.DATE,
     allowNull: false,
+    validate: {
+      zeroOr30Min(value) {
+        const date = new Date(value);
+        if (!(date.getMilliseconds() === 0
+            && date.getSeconds() === 0
+            && (date.getMinutes() === 0 || date.getMinutes() === 30))) {
+          throw new Error('startDateTime\'s time must have format hh:00:00 or hh:30:00');
+        }
+      },
+    },
   },
   endDateTime: {
     type: Sequelize.DATE,
     allowNull: false,
+    validate: {
+      zeroOr30Min(value) {
+        const date = new Date(value);
+        if (!(date.getMilliseconds() === 0
+            && date.getSeconds() === 0
+            && (date.getMinutes() === 0 || date.getMinutes() === 30))) {
+          throw new Error('startDateTime\'s time must have format hh:00:00 or hh:30:00');
+        }
+      },
+    },
   },
   reason: {
     type: Sequelize.TEXT,
