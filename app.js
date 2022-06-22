490783a62c096891602300db1e3f84381ca17bb4
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-22 18:45:27 +0200

    Create relations between the models

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
+const sequelize = require('./util/database');
+const Appointment = require('./models/appointment');
+const ClientGuest = require('./models/client-guest');
+
 const app = express();
 
 app.use((req, res, next) => {
   console.log('handling http request...');
   next();
 })
+
+Appointment.belongsTo(ClientGuest);
+ClientGuest.hasOne(Appointment);
 
 app.listen(3000);
