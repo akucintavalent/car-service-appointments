d5c0f024ab9067e822141b4e84ca9b1b83ab6d3a
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-22 19:11:13 +0200

    Add validation to ClientGuest model

--- models/client-guest.js
+++ models/client-guest.js
@@ -12,14 +12,23 @@ const ClientGuest = sequelize.define('clientGuest', {
   firstName: {
     type: Sequelize.STRING,
     allowNull: false,
+    validate: {
+      is: /^[a-zA-Z]+$/i
+    },
   },
   lastName: {
     type: Sequelize.STRING,
     allowNull: false,
+    validate: {
+      is: /^[a-zA-Z]+$/i
+    },
   },
   phoneNumber: {
     type: Sequelize.STRING,
     allowNull: false,
+    validate: {
+      is: /^[\+]?\d{6,12}$/g
+    },
   },
 });
 
