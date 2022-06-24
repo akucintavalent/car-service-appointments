f30510f58b50a51df81b2862d86f41255f29e991
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-24 12:46:44 +0200

    Provide possibility to use a different database for testing purposes

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME || 'car-service', process.env.DB_USER || 'root', process.env.DB_PASSWORD || 'password', {
+const sequelize = new Sequelize(process.env.TEST_DB_NAME || process.env.DB_NAME || 'car-service', process.env.DB_USER || 'root', process.env.DB_PASSWORD || 'password', {
   dialect: 'mysql',
   host: process.env.DB_HOST || 'localhost',
 });
